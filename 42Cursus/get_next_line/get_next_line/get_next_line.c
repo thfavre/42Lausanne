3d71b15ca8a8dc2ea943b077f59a93b0c508c6fe
thfavre <thfavre@student.42lausanne.ch>
2022-11-17 11:05:13 +0100

    get_next_line working, NULL to add if error

--- 42Cursus/get_next_line/get_next_line/get_next_line.c
+++ 42Cursus/get_next_line/get_next_line/get_next_line.c
@@ -4,35 +4,40 @@ int	is_line_in_stash(t_list *stash);
 int	add_buffer_to_stash(t_list **stash, int fd);
 int	get_line_len(t_list *stash);
 char	*create_line(t_list *stash, int line_len);
-void	clean_stash(t_list **stash);
+void	clean_stash(t_list *stash);
 
 char	*get_next_line(int fd)
 {
 	static t_list	*stash = NULL;  // reserve
 	int		line_len;
 	char	*line;
-
+	int		add_buffer_output;
 	// 1) check if a '\n' is in the stash (can only in the first one if any)
-	printf("mem %p\n", stash);
-	if (stash != NULL)
-		printf("1) First el : %s", stash->content);
+	//if (stash != NULL)
 	if (!is_line_in_stash(stash))
 	{
 		// 2) if TRUE : skip this step 
 		//		add a buffer to the stash while the buffer does not contain a '\n' (or fd is finished)
 		//add_buffer_to_stash(&stash, fd);
 		while (add_buffer_to_stash(&stash, fd) == 0)
-			;
+		{
+			add_buffer_output = add_buffer_to_stash(&stash, fd);
+			//printf("a : %d\n", add_buffer_output);
+			if (add_buffer_output == -1)
+				return (NULL);
+			if (add_buffer_output == 1)
+				break ;
+		}
 	}
-	printf("\t2) First el : %s\n", stash->content);
+	//printf("s : '%s'\n", stash->next->content);
+	//if (stash->content[0] == 0)
+	//	return (NULL);
 	// 3) count the size of the line
 	line_len = get_line_len(stash);
-	printf("line len : %d\n", line_len);
 	// 4) create with malloc the string containing the line
 	line = create_line(stash, line_len);
-	printf("\n LINE : %s\n", line);
 	// 5) clear all the stash except the last one if he contain a '\n' 
-	clean_stash(&stash);
+	clean_stash(stash);
 	return (line);
 }
 
@@ -46,7 +51,7 @@ int	is_line_in_stash(t_list *stash)
 	i = 0;
 	stash_content = stash->content;
 	while (stash_content[i])
-		if (stash_content[i] == '\n')
+		if (stash_content[i++] == '\n')
 			return (1);
 	return (0);
 }
@@ -60,12 +65,11 @@ int	add_buffer_to_stash(t_list **stash, int fd)
 	buffer = malloc(sizeof(*buffer) * (BUFFER_SIZE + 1));
 	//check malloc 
 	read_output = read(fd, buffer, BUFFER_SIZE);
-	printf("rd : %d, str : <%s>  \n", read_output, buffer);
 	if (read_output == -1)
-		return (-1);
-	if (read_output < BUFFER_SIZE)
-		return (1);
-	buffer[BUFFER_SIZE] = '\0';
+		return (-1); // error
+	if (read_output == 0)
+		return (-1); // nothing to read
+	buffer[read_output] = '\0';
 	ft_lstadd_back(stash, ft_lstnew(buffer));
 	i = 0;
 	while (buffer[i])
@@ -85,7 +89,6 @@ int	get_line_len(t_list *stash)
 		i = 0;
 		while ((stash->content)[i])
 		{
-			//printf(" len:%d ", len);
 			if ((stash->content)[i] == '\n')
 				return (len);
 			len++;
@@ -122,10 +125,58 @@ char	*create_line(t_list *stash, int line_len)
 		}
 		stash = stash->next;
 	}
+	return (line);
 }
 
-void	clean_stash(t_list **stash)
+void	clean_stash(t_list *stash)
 {
+	/*
+	t_list *new_stash = ft_lstnew(" NEW ");
+	*stash = *new_stash;
+	*/
+	t_list	*last;
+	int		i;
+	int		j;
+	int		len;
+	char	*str;
+	t_list	*stash_ptr;
+
+	stash_ptr = stash;
+	last = ft_lstlast(stash);
+	i = 0;
+	while ((last->content)[i] && (last->content)[i] != '\n')
+		i++;
+	i++;
+	len = 0;
+	while ((last->content)[i + len])
+		len++;
+	str = malloc(sizeof(*str) * (len + 1));
+	j = 0;
+	while (j < len)
+	{
+		str[j] = (last->content)[i + j];
+		j++;
+	}
+	str[j] = '\0';
+	
+	
+	// last is next
+	while (stash)
+	{
+		last = (stash)->next;
+		free(stash->content);
+		//ft_lstdelone(*lst, del);
+		stash = last;
+	}
+	free(stash);
+	//free(*lst);
+	//*lst = NULL;	
+
+
+	*stash_ptr = *ft_lstnew(str);
+
+
+	/*
 	t_list	*new_stash;
 	char	*new_stash_content;
 	int		i;
@@ -139,29 +190,22 @@ void	clean_stash(t_list **stash)
 	while ((last->content)[i] != '\n')
 		i++;
 	//new_stash_content = last->content + i;
-	/*new_stash_len = 0;
+	new_stash_len = 0;
 	j = 0;
 	while ((last->content)[i + j++])
 		new_stash_len++;
 	new_stash
-	*/
 	new_stash = ft_lstnew(last->content + i + 1);
 
-	printf("new stash content : %s \n", new_stash->content);
-	printf("mem %p\n", stash);
 	while ((*stash))
 	{
 		next = (*stash)->next;
-		printf(" + 1 ");
 		free((*stash)->content);
 		free(*stash);
 		*stash = next;
 	}
 	stash = &new_stash;
-	printf("mem %p\n", stash);
-	printf("The new stash : %s", (*stash)->content);
 
-	/*
 	while (str[i])
 	{
 		lnew_stash_len++;
@@ -195,11 +239,10 @@ void	clean_stash(t_list **stash)
 int	main()
 {
 	int	fd = open("file.txt", O_RDONLY);
-	printf("\n_____\nOUTPUT :{%s}\n____\n", get_next_line(fd));
-	printf("\n_____\nOUTPUT :{%s}\n____\n", get_next_line(fd));
-	printf("\n_____\nOUTPUT :{%s}\n____\n", get_next_line(fd));
-	printf("\n_____\nOUTPUT :{%s}\n____\n", get_next_line(fd));
-	printf("\n_____\nOUTPUT :{%s}\n____\n", get_next_line(fd));
+	int i = 0;
+	while (i++ < 10)
+		printf("->{%s}\n\n", get_next_line(fd));
+
 
 }
 
