522c13e6c843ab62610a6c7fe6eea20fba540dcc
thfavre <thomas.favre22@yahoo.fr>
2023-04-29 12:17:40 +0200

    main

--- 42Cursus/CPP/CPP01/ex04/main.cpp
+++ 42Cursus/CPP/CPP01/ex04/main.cpp
@@ -16,10 +16,22 @@ std::string	readFile(std::ifstream& file) {
 	return (content);
 }
 
-std::string replace(std::string str, std::string s1, std::string s2) {
+std::string	replace(std::string str, std::string s1, std::string s2) {
 	std::string	new_string;
 	size_t		pos;
 
+	pos = 0;
+	while ((pos = str.find(s1, pos)) != std::string::npos)
+	{
+
+		// new_string += str.substr(0, pos);
+		// new_string += s2;
+		// str.erase(0, pos + s1.length());
+		// std::cout << "str : " << str << std::endl;
+		// std::cout << "pos :"<< pos << std::endl;
+		// std::cout << "new :"<< new_string << std::endl << std::endl << std::endl;
+	}
+	return (new_string);
 }
 
 int	main(int argc, char **argv) {
@@ -41,8 +53,12 @@ int	main(int argc, char **argv) {
 		return (1);
 	}
 
-	outfile.open((std::string)argv[1] + (std::string)".replace");
-	// check error
+	outfile.open(((std::string)argv[1] + ".replace").c_str());
+	if (outfile.fail())
+	{
+		std::cout << "Unable to open the file: " << argv[1] << ".replace" << std::endl;
+		return (1);
+	}
 	infileContent = readFile(infile);
 
 	outfile << replace(infileContent, argv[2], argv[3]);
