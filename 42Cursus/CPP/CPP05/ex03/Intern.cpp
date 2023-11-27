eaedb1ba4d94b66ad78eb19b25584e1ea15c4eba
thfavre <thfavre@student.42lausanne.ch>
2023-11-27 16:43:47 +0100

    map removed from CPP05 ex03

--- 42Cursus/CPP/CPP05/ex03/Intern.cpp
+++ 42Cursus/CPP/CPP05/ex03/Intern.cpp
@@ -1,10 +1,8 @@
-#include <map>
 #include "Intern.hpp"
 #include "AForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
-
 
 Intern::Intern()
 {
@@ -30,19 +28,30 @@ AForm *Intern::makeForm(std::string name, std::string target)
 {
 	AForm *form;
 
-	//map
-	std::map<std::string, AForm *> forms = {
-		{"robotomy request", new PresidentialPardonForm(target)},
-		{"presidential pardon", new RobotomyRequestForm(target)},
-		{"shrubbery creation", new ShrubberyCreationForm(target)}
+	AForm *forms[] = {
+		new PresidentialPardonForm(target), // robotomy request
+		new RobotomyRequestForm(target),	// presidential pardon
+		new ShrubberyCreationForm(target)	// shrubbery creation
 	};
 
-	if (forms.find(name) == forms.end())
-		throw FormNotFoundException();
-	form = forms[name];
+	std::string formNames[] = {
+		"robotomy request",
+		"presidential pardon",
+		"shrubbery creation"};
 
+	int numForms = sizeof(forms) / sizeof(forms[0]);
+
+	for (int i = 0; i < numForms; i++)
+	{
+		if (formNames[i] == name)
+		{
+			form = forms[i];
+			break;
+		}
+	}
+
+	if (!form)
+		throw FormNotFoundException();
 
 	return form;
 }
-
-
