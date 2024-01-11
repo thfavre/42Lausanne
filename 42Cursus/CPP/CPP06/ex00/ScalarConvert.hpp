9d26ceb977062021a6692585bc965f190ec7ae18
thfavre <thfavre@student.42lausanne.ch>
2024-01-11 23:25:40 +0100

    CPP06 started

---
+++ 42Cursus/CPP/CPP06/ex00/ScalarConvert.hpp
@@ -0,0 +1,16 @@
+#ifndef SCALARCONVERT_HPP
+# define SCALARCONVERT_HPP
+
+#include <string>
+
+class ScalarConvert
+{
+public:
+	static void convert(std::string string);
+private:
+	ScalarConvert();
+	~ScalarConvert();
+};
+
+
+#endif // SCALARCONVERT_HPP
