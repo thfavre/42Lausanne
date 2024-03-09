b55f8de9ca535f9c5da5143bf080380dd6f0d17a
thfavre <thfavre@student.42lausanne.ch>
2024-03-09 12:47:32 +0100

    CPP 08 done

--- 42Cursus/CPP/CPP08/ex02/MutantStack.hpp
+++ 42Cursus/CPP/CPP08/ex02/MutantStack.hpp
@@ -1,21 +1,38 @@
 #ifndef MUTANT_STACK_HPP
 # define MUTANT_STACK_HPP
 
+#include <iostream>
 #include <stack>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
 	public:
-		MutantStack();
-		MutantStack(const MutantStack &mutantStack)
-		~MutantStack();
+		MutantStack<T>() {}
+
+		MutantStack<T>(const MutantStack &mutantStack)
+		{
+			*this=mutantStack;
+		}
+
+		~MutantStack<T>() {}
+
+		MutantStack<T>	&operator=(const MutantStack<T> &ms)
+		{
+			*this = ms;
+			return (*this);
+		}
 
 		typedef typename std::stack<T>::container_type::iterator iterator;
-		iterator	begin(void);
-		iterator	end(void);
+		iterator	begin(void)
+		{
+			return (this->c.begin());
+		}
 
-
+		iterator	end(void)
+		{
+			return (this->c.end());
+		}
 };
 
 
---
+++ 42Cursus/CPP/CPP08/ex02/main.cpp
@@ -0,0 +1,52 @@
+#include "MutantStack.hpp"
+#include <list>
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << "\n-----Now with list-----" << std::endl;
+
+	std::list<int> l;
+	l.push_back(5);
+	l.push_back(17);
+	std::cout << l.back() << std::endl;
+	l.pop_back();
+	std::cout << l.size() << std::endl;
+	l.push_back(3);
+	l.push_back(5);
+	l.push_back(737);
+	//[...]
+	l.push_back(0);
+	std::list<int>::iterator itl = l.begin();
+	std::list<int>::iterator itel = l.end();
+	++it;
+	--it;
+	while (itl != itel)
+	{
+		std::cout << *itl << std::endl;
+		++itl;
+	}
+	std::stack<int> s2(mstack);
+	return 0;
+}
