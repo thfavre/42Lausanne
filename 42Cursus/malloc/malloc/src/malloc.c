e417643b7b7e01b20373f7c162758014e77a50b2
thfavre <thfavre@student.42lausanne.ch>
2024-09-07 00:10:39 +0200

    minor changes to makefiile

--- 42Cursus/malloc/malloc/src/malloc.c
+++ 42Cursus/malloc/malloc/src/malloc.c
@@ -6,10 +6,11 @@ int main(int argc, char **argv) {
 	printf("Hello, world!\n");
 	(void)argc;
 	(void)argv;
-	  void *p = malloc(1024);
-  if (p == NULL) {
-	return 1;
-  }
-  free(p);
-	return 1;
+// 	  void *p = malloc(1024);
+//   if (p == NULL) {
+// 	return 1;
+//   }
+//   free(p);
+// 	return 1;
+return 1;
 }
