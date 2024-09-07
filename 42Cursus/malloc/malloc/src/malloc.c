87a736ee000a29a3fd1a7175e5570716b8bc7d1e
Thomas <thomas.favre22@yahoo.fr>
2024-09-07 02:59:40 +0200

    test

--- 42Cursus/malloc/malloc/src/malloc.c
+++ 42Cursus/malloc/malloc/src/malloc.c
@@ -1,4 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
 
-int main() {
+
+int main(int argc, char **argv) {
+	printf("Hello, world!\n");
+	(void)argc;
+	(void)argv;
+	  void *p = malloc(1024);
+  if (p == NULL) {
+	return 1;
+  }
+  free(p);
 	return 1;
 }
